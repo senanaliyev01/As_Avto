6cd7a4eaef270aa4ab200bc43e43408ed1e4d8ab
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-10 12:01:20 +0400

    register_funksiyasi

--- istifadeciler/urls.py
+++ istifadeciler/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('password-change/', views.password_change, name='password_change'),
     path('logout/', views.logout_view, name='logout'),
-    path('messages/<int:receiver_id>/', views.get_messages, name='get_messages'),
-    path('send-message/', views.send_message, name='send_message'),
-    path('chat-users/', views.get_chat_users, name='get_chat_users'),
+    path('api/chat/users/', views.get_chat_users, name='get_chat_users'),
+    path('api/chat/messages/<int:receiver_id>/', views.get_messages, name='get_messages'),
+    path('api/chat/send/', views.send_message, name='send_message'),
 ]

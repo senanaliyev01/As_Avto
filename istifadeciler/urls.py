9d56573d5ab4764c04977988918cc9e1ad0287eb
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-11 20:24:39 +0400

    hazir_sayt

--- istifadeciler/urls.py
+++ istifadeciler/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('Giriş/', views.login_view, name='login'),
-    path('Qeydiyyat/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('Hesab/', views.profile, name='profile'),
     path('Şifrə_Dəyişilməsi/', views.password_change, name='password_change'),
     path('Çıxış/', views.logout_view, name='logout'),

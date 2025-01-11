f904d2d11c785430ee90b38879d84a46a23e49dd
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-11 15:00:36 +0400

    chat_repair

--- istifadeciler/admin.py
+++ istifadeciler/admin.py
@@ -8,19 +8,4 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'ad', 'soyad', 'telefon')
     list_editable = ('is_approved',)
 
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'short_content', 'message_type', 'created_at', 'is_read', 'is_delivered')
-    list_filter = ('message_type', 'is_read', 'is_delivered', 'created_at')
-    search_fields = ('sender__username', 'receiver__username', 'content')
-    readonly_fields = ('created_at',)
-    ordering = ('-created_at',)
 
-    def short_content(self, obj):
-        if obj.message_type == 'audio':
-            return 'Audio Message'
-        elif obj.content:
-            return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
-        return 'Empty message'
-    
-    short_content.short_description = 'Message Content'
--- istifadeciler/models.py
+++ istifadeciler/models.py
@@ -27,22 +27,3 @@ def save_user_profile(sender, instance, **kwargs):
     except Profile.DoesNotExist:
         Profile.objects.create(user=instance)
 
-class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    content = models.TextField(blank=True, null=True)
-    audio_file = models.FileField(upload_to='chat_audio/', blank=True, null=True)
-    message_type = models.CharField(max_length=10, default='text', choices=[
-        ('text', 'Text'),
-        ('audio', 'Audio')
-    ])
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-    is_delivered = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['created_at']
-
-    def __str__(self):
-        return f'Message from {self.sender.username} to {self.receiver.username}'
-

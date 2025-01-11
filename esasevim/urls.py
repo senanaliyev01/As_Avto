01548ed2cb7e6d41a8806c9879c14411141d39d2
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-11 19:50:10 +0400

    Url_Duzelisi

--- esasevim/urls.py
+++ esasevim/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('Əsas Səhifə/', views.esasevim, name='main'),
+    path('Əsas_Səhifə/', views.esasevim, name='main'),
     path('get_statistics/', views.get_statistics, name='get_statistics'),
     path('Rəy-əlavə-et/', views.rey_elave_et, name='rey_elave_et'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- istifadeciler/urls.py
+++ istifadeciler/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('Giriş/', views.login_view, name='login'),
     path('Qeydiyyat/', views.register, name='register'),
     path('Hesab/', views.profile, name='profile'),
-    path('Şifrə Dəyişilməsi/', views.password_change, name='password_change'),
+    path('Şifrə_Dəyişilməsi/', views.password_change, name='password_change'),
     path('Çıxış/', views.logout_view, name='logout'),
 ]

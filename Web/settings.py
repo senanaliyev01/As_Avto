587ab1b562b246e9ebdadbd2a0525c178c141334
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-24 14:13:14 +0400

    hazir12334

--- Web/settings.py
+++ Web/settings.py
@@ -164,7 +164,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# Default primary key field type
+#11 Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

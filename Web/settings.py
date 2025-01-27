a33d197ffb7b0efe38ec0080ce009ee94c566e0b
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-27 19:41:33 +0400

    404-html

--- Web/settings.py
+++ Web/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG')
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get('DEBUG')
+
 
 ALLOWED_HOSTS = ['188.245.112.154','localost','127.0.0.1','0.0.0.0', 'as-avto.com', 'www.as-avto.com']
 # ALLOWED_HOSTS = ['*']

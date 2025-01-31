8a82c5f6b227fcad986035523d68cccad662bc08
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-31 11:54:52 +0400

    Rent-A-Car

--- rentacar/admin.py
+++ rentacar/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Car, CarImage
 
+class CarImageInline(admin.TabularInline):  # Inline sinifi
+    model = Car.images.through  # ManyToManyField üçün əlaqə modeli
+    extra = 1  # Yeni şəkil əlavə etmək üçün boş forma
+
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ('name', 'engine_capacity', 'car_type', 'sale_price', 'rent_price')
     search_fields = ('name',)
     list_filter = ('car_type',)
-    filter_horizontal = ('images',)
+    inlines = [CarImageInline]  # Inline əlavə edildi
 
 @admin.register(CarImage)
 class CarImageAdmin(admin.ModelAdmin):

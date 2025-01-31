0c884ceb8bde6058f93b68ce922801af3fbc2153
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-01-31 11:37:21 +0400

    Rent-A-Car

--- static/js/loading.js
+++ static/js/loading.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "/istifadeciler/password_change/",
                 "/istifadeciler/register/",
                 "/istifadeciler/login/",
-                /rent-a-car/,
+                "/rent-a-car/",
             ],
             baseUrl: "https://as-avto.com"
         },

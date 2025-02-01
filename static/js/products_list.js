dec5f4369a11f55e8c2fa98dad61f04724fe39b9
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-01 16:08:16 +0400

    JS AXTAR

--- static/js/products_list.js
+++ static/js/products_list.js
@@ -1,26 +1,26 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const searchForm = document.querySelector('form');
-//     const searchButton = document.getElementById('search-button');
-//     const buttonText = searchButton.querySelector('.button-text');
-//     const spinner = searchButton.querySelector('.spinner');
+document.addEventListener('DOMContentLoaded', function() {
+    const searchForm = document.querySelector('form');
+    const searchButton = document.getElementById('search-button');
+    const buttonText = searchButton.querySelector('.button-text');
+    const spinner = searchButton.querySelector('.spinner');
 
-//     searchForm.addEventListener('submit', function(e) {
-//         e.preventDefault(); // Formanın default davranışını dayandırıq
+    searchForm.addEventListener('submit', function(e) {
+        e.preventDefault(); // Formanın default davranışını dayandırıq
 
-//         // Buttonu loading vəziyyətinə keçiririk
-//         searchButton.classList.add('loading');
-//         buttonText.style.opacity = '0.5';
-//         spinner.style.display = 'inline-block';
+        // Buttonu loading vəziyyətinə keçiririk
+        searchButton.classList.add('loading');
+        buttonText.style.opacity = '0.5';
+        spinner.style.display = 'inline-block';
 
-//         // 2 saniyə gözləyirik
-//         setTimeout(() => {
-//             // 2 saniyədən sonra formanı göndəririk
-//             searchButton.classList.remove('loading');
-//             buttonText.style.opacity = '1';
-//             spinner.style.display = 'none';
+        // 2 saniyə gözləyirik
+        setTimeout(() => {
+            // 2 saniyədən sonra formanı göndəririk
+            searchButton.classList.remove('loading');
+            buttonText.style.opacity = '1';
+            spinner.style.display = 'none';
             
-//             // Formanı göndəririk
-//             this.submit();
-//         }, 2000);
-//     });
-// });
+            // Formanı göndəririk
+            this.submit();
+        }, 2000);
+    });
+});

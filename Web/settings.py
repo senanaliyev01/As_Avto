0b52bd50d3c727d396c37be2c8729efd2288b5d4
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-03 20:31:30 +0400

    404_Handler

--- Web/settings.py
+++ Web/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'as-avto.middlewares.Force404Middleware',
 ]
 
 ROOT_URLCONF = 'Web.urls'
--- Web/urls.py
+++ Web/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin-as-avto-1983-2002/', admin.site.urls),
@@ -16,3 +17,10 @@ urlpatterns = [
 admin.site.site_title = 'AS-AVTO'
 admin.site.site_header = 'Admin AS-AVTO'
 admin.site.index_title = 'Admin Panel'
+
+
+
+def custom_404_view(request, exception=None):
+    return render(request, "404.html", status=404)
+
+handler404 = custom_404_view

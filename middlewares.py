aa0d0bcdf90a35af0dc42742cb70a5b81805f7bb
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-03 20:44:01 +0400

    404_Handler

--- middlewares.py
+++ middlewares.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponseNotFound
 
-class Force404Middleware(MiddlewareMixin):
-    def process_response(self, request, response):
+class Force404Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
         if response.status_code == 404:
-            return render(request, "404.html", status=404)
+            return HttpResponseNotFound("<h1>404 Not Found - Səhifə tapılmadı</h1>")
         return response

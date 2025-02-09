0b2abe9b829f0fa7d01cb24bc55d801628348718
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-09 13:58:48 +0400

    Sebet Linki Qiymet

--- Web/middlewares.py
+++ Web/middlewares.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.utils.deprecation import MiddlewareMixin
-from mehsullar.models import Kateqoriya, Brend, Marka, Sebet
-from django.db.models import Sum, F
 
 class Force404Middleware(MiddlewareMixin):
     def process_response(self, request, response):
@@ -9,16 +7,11 @@ class Force404Middleware(MiddlewareMixin):
             return render(request, "404.html", status=404)
         return response
 
+from mehsullar.models import Kateqoriya, Brend, Marka
+
 class AddSearchDataMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # Axtarış üçün lazım olan məlumatları əldə edin
         request.kateqoriyalar = Kateqoriya.objects.all()
         request.brendler = Brend.objects.all()
         request.markalar = Marka.objects.all()
-
-class AddCartTotalMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        if request.user.is_authenticated:
-            # İstifadəçinin səbətindəki cəmi məbləği hesablayırıq
-            cart_total = Sebet.objects.filter(user=request.user).aggregate(total=Sum(F('miqdar') * F('mehsul__qiymet')))['total'] or 0
-            request.cart_total = cart_total  # Cəmi məbləği request obyektinə əlavə edirik

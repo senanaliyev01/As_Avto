ae2d7b0d5f0e3f602dc4a9cc203bfa5a9a3440a2
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-12 19:33:37 +0400

    Admin_Paneli_Jet

--- Web/urls.py
+++ Web/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+     path('jet/', include('jet.urls', 'jet')),
     path('admin-as-avto-1983-2002/', admin.site.urls),
     path('istifadeciler/', include('istifadeciler.urls')),
     path('', include('anaevim.urls')),

b22ac80e027460e4ada0c7236991f7f341f9f05e
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-14 20:18:56 +0400

    Ready_Website

--- anaevim/urls.py
+++ anaevim/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.anaevim, name='anaevim'),  # Əsas səhifə
-    path('mehsul/<int:mehsul_id>/', views.mehsul_etrafli, name='mehsul_etrafli'),  # Məhsul haqqında ətraflı
+    path('product/<int:mehsul_id>/', views.mehsul_etrafli, name='mehsul_etrafli'),  # Məhsul haqqında ətraflı
 ]

4167762e3deadecf9b3aecccbeaea9cc7b20c3d8
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-14 15:00:19 +0400

    home.html sekil slide

--- anaevim/views.py
+++ anaevim/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from mehsullar.models import Brend, Marka, Mehsul, MarkaSekil
 
-def anaevim(request):
-    return render(request, 'home.html')
+def home(request):
+    brendler = Brend.objects.all()
+    markalar = Marka.objects.all()
+    mehsullar = Mehsul.objects.all()
+    marka_sekiller = MarkaSekil.objects.all()
+    
+    context = {
+        'brendler': brendler,
+        'markalar': markalar,
+        'mehsullar': mehsullar,
+        'marka_sekiller': marka_sekiller,
+    }
+    
+    return render(request, 'home.html', context)

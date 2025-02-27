cb4fa9d9ff23f3fe8da34b129ce6c3a45c65f631
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-02-27 15:29:51 +0400

    Main Hero Slider

--- esasevim/admin.py
+++ esasevim/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import HeroSlider
+from .models import slider
 
-@admin.register(HeroSlider)
-class HeroSliderAdmin(admin.ModelAdmin):
+@admin.register(slider)
+class sliderAdmin(admin.ModelAdmin):
     list_display = ('basliq',)
     search_fields = ['basliq', 'metn']

9f2465f735edcc106025b3ac557f2a97a5e107ef
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-12 18:26:13 +0400

    Ready_Website

--- static/js/base.js
+++ static/js/base.js
@@ -1091,12 +1091,6 @@
                                 </div>
                             </div>
                             <div class="cart-item-price">${item.price} ₼</div>
-                             <form action="{% url 'sebetden_sil' item.id %}" method="post" style="display: inline;">
-                            {% csrf_token %}
-                            <button type="submit" class="delete-btn" title="Sil" onclick="return confirm('Məhsulu silmək istədiyinizə əminsiniz?')">
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
-                        </form>
                         </div>
                     `;
                 });

1a99816bc55e89fd574a4549fc1cb7144800c92b
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-14 13:01:36 +0400

    Her Slide Null Blank

--- esasevim/models.py
+++ esasevim/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class HeroSlide(models.Model):
     sekil = models.ImageField(upload_to='hero_slides/', verbose_name='Şəkil')
-    basliq = models.CharField(max_length=200, verbose_name='Başlıq')
-    alt_basliq = models.CharField(max_length=300, verbose_name='Alt Başlıq')
+    basliq = models.CharField(max_length=200, verbose_name='Başlıq', null=True, blank=True)
+    alt_basliq = models.CharField(max_length=300, verbose_name='Alt Başlıq', null=True, blank=True)
     yaradilma_tarixi = models.DateTimeField(auto_now_add=True)
     
     class Meta:

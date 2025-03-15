4709dd01241eaf321bd66124d242b001b764e4c8
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-15 12:10:34 +0400

    Axtaris FUnksiyalari

--- Web/settings.py
+++ Web/settings.py
@@ -539,7 +539,7 @@ CHANNEL_LAYERS = {
 # Elasticsearch konfiqurasiyası
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'https://188.245.112.154:8000'
+        'hosts': 'http://188.245.112.154:9200'
     },
 }
 

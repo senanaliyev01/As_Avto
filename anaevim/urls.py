a94de173383752bf8432542a1610658698d9bc9c
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-27 13:56:27 +0400

    axtaris formu

--- anaevim/urls.py
+++ anaevim/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.anaevim, name='anaevim'),  # Əsas səhifə
     path('product/<int:mehsul_id>/', views.mehsul_etrafli, name='mehsul_etrafli_sade'),  # Sadə ID ilə
-    path('product/<str:mehsul_adi>-<str:mehsul_oem>-<str:mehsul_brend_kod>/<int:mehsul_id>/', 
+    path('product/<str:mehsul_adi>-<str:mehsul_oem>-<str:mehsul_brend_kod>/', 
          views.mehsul_etrafli, 
          name='mehsul_etrafli'),  # Məhsul haqqında ətraflı
 ]

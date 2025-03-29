e09485be37545bfceabdd16546934665bf3c2cd5
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-29 20:41:39 +0400

    Ready_Website-FULL

--- Web/sitemaps.py
+++ Web/sitemaps.py
@@ -4,6 +4,7 @@ from mehsullar.models import Mehsul
 from django.utils import timezone
 from django.utils.text import slugify
 from urllib.parse import quote
+import re
 
 class StaticViewSitemap(Sitemap):
     changefreq = "daily"
@@ -32,14 +33,23 @@ class MehsulSitemap(Sitemap):
 
     def location(self, obj):
         # URL-dəki xüsusi simvolları düzgün kodlaşdırırıq
-        encoded_name = quote(obj.adi)
-        encoded_oem = quote(obj.oem)
-        encoded_brand_code = quote(obj.brend_kod)
+        # məhsul adında və kodlarda olan xüsusi simvollar düzgün işləməyə bilər
+        # əvvəlcə slugify və sonra URL encoding tətbiq edək
+        
+        # Xüsusi simvolları temizləyək
+        clean_name = re.sub(r'[^\w\s-]', '', str(obj.adi)) if obj.adi else ''
+        clean_oem = re.sub(r'[^\w\s-]', '', str(obj.oem)) if obj.oem else ''
+        clean_brand_code = re.sub(r'[^\w\s-]', '', str(obj.brend_kod)) if obj.brend_kod else ''
+        
+        # Slugify edək
+        slug_name = slugify(clean_name)
+        slug_oem = slugify(clean_oem)
+        slug_brand_code = slugify(clean_brand_code)
         
         return reverse('mehsul_etrafli', kwargs={
-            'mehsul_adi': encoded_name,
-            'mehsul_oem': encoded_oem,
-            'mehsul_brend_kod': encoded_brand_code,
+            'mehsul_adi': slug_name,
+            'mehsul_oem': slug_oem,
+            'mehsul_brend_kod': slug_brand_code,
             'mehsul_id': obj.id
         })
 
@@ -69,12 +79,20 @@ class MehsulSitemap(Sitemap):
                 'priority': str(priority if priority is not None else ''),
             }
 
-            # Şəkil məlumatlarını əlavə edirik
+            # Şəkil məlumatlarını əlavə edirik, xüsusi simvolları təmizləyərək
             if hasattr(item, 'sekil') and item.sekil:
+                # Xüsusi simvolları təmizləyirik ki JSON formatlaşdırma xətası olmasın
+                safe_title = re.sub(r'[^\w\s-]', '', str(item.adi)) if item.adi else ''
+                safe_brend = re.sub(r'[^\w\s-]', '', str(item.brend.adi)) if item.brend and item.brend.adi else ''
+                safe_brend_kod = re.sub(r'[^\w\s-]', '', str(item.brend_kod)) if item.brend_kod else ''
+                safe_oem = re.sub(r'[^\w\s-]', '', str(item.oem)) if item.oem else ''
+                
+                caption = f"{safe_title} - {safe_brend} - {safe_brend_kod} - {safe_oem}"
+                
                 url_info['images'] = [{
                     'loc': f"{protocol}://{domain}{item.sekil.url}",
-                    'title': item.adi,
-                    'caption': f"{item.adi} - {item.brend.adi} - {item.brend_kod} - {item.oem}"
+                    'title': safe_title,
+                    'caption': caption
                 }]
 
             urls.append(url_info)

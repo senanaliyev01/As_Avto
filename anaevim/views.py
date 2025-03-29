affcd7d23d37f9cc1910e9e382cc603cdb180874
Senan Aliyev <senan.eliyev.9997@gmail.com>
2025-03-29 13:45:41 +0400

    ready

--- anaevim/views.py
+++ anaevim/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from mehsullar.models import Brend, Marka, Mehsul, MarkaSekil, Kateqoriya, OEMKod
+from mehsullar.models import Brend, Marka, Mehsul, MarkaSekil
 from django.template.defaultfilters import slugify
-from django.db.models import Q
-import re
 
 def anaevim(request):
     brendler = Brend.objects.all()
@@ -17,33 +15,6 @@ def anaevim(request):
     }
     
     return render(request, 'home.html', context)
-
-def mehsullar(request):
-    # Bütün məhsulları, kateqoriyaları, brendləri və markaları əldə etmək
-    mehsullar = Mehsul.objects.all()
-    kateqoriyalar = Kateqoriya.objects.all()
-    brendler = Brend.objects.all()
-    markalar = Marka.objects.all()
-    
-    # Axtarış sorğusu varsa
-    search_text = request.GET.get('search_text', '')
-    if search_text:
-        # Xüsusi simvolları təmizlə (əlavə OEM kodları üçün)
-        clean_search = re.sub(r'[^a-zA-Z0-9]', '', search_text)
-        
-        # OEM kodlarında axtarış
-        mehsullar = Mehsul.objects.filter(
-            Q(oem_kodlar__kod__icontains=clean_search)  # OEM kodları ilə axtarış
-        ).distinct()
-    
-    context = {
-        'mehsullar': mehsullar,
-        'kateqoriyalar': kateqoriyalar,
-        'brendler': brendler,
-        'markalar': markalar,
-    }
-    
-    return render(request, 'mehsullar.html', context)
 
 def mehsul_etrafli(request, mehsul_id, mehsul_adi=None, mehsul_oem=None, mehsul_brend_kod=None):
     mehsul = get_object_or_404(Mehsul, id=mehsul_id)

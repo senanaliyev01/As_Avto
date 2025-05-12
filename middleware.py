2e10b76887b7164edb54f2f8f9f91a8eaa2011f3
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-05-12 10:58:42 +0400

    404 html

---
+++ middleware.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound
+
+class Custom404Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code == 404:
+            return render(request, '404.html', status=404)
+        return response 

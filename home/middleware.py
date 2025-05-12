653746f4fed1e44f7a38e841b3f15f3d6feb07d5
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-05-12 11:21:36 +0400

    qurulur

--- home/middleware.py
+++ home/middleware.py
@@ -1,7 +1,3 @@
-from django.conf import settings
-from django.http import Http404
-from django.template.loader import render_to_string
-from django.http import HttpResponse
 from .models import Sifaris, Kateqoriya, Firma, Avtomobil
 
 class GlobalDataMiddleware:
@@ -20,31 +16,4 @@ class GlobalDataMiddleware:
             request.models = Avtomobil.objects.all()
 
         response = self.get_response(request)
-        return response
-
-class Custom404Middleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        
-        if response.status_code == 404 and not settings.DEBUG:
-            try:
-                # 404.html səhifəsini render edirik
-                html = render_to_string('404.html', {}, request)
-                return HttpResponse(html, status=404)
-            except:
-                # Əgər 404.html tapılmazsa, sadə bir mesaj göstəririk
-                return HttpResponse('Səhifə tapılmadı', status=404)
-                
-        return response
-
-    def process_exception(self, request, exception):
-        if isinstance(exception, Http404) and not settings.DEBUG:
-            try:
-                html = render_to_string('404.html', {}, request)
-                return HttpResponse(html, status=404)
-            except:
-                return HttpResponse('Səhifə tapılmadı', status=404)
-        return None 
+        return response 

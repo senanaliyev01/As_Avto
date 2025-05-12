4d5cd5ff8ea8b277eec9c27b61378ca39fd52a70
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-05-12 12:45:03 +0400

    qurulur

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sifarisler/', views.orders_view, name='orders'),
-    path('sifaris/<int:order_id>/', views.order_detail_view, name='order_detail'),
-] 

13471ba1bdb006cac3e3582f77394afdb19de241
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-06-24 17:14:16 +0400

    YENIDIR TRUE 10 SANIYE

--- home/models.py
+++ home/models.py
@@ -10,6 +10,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import threading
 import time
+import home.signals
 
 class Header_Message(models.Model):
     mesaj = models.CharField(max_length=100)

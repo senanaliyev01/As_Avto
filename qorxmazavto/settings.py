54f5e6a879f4abc8905f5eba83d7f755761d0240
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-02 16:55:59 +0400

    GUNICORN HAZIRDIR 5 WORKERS

--- qorxmazavto/settings.py
+++ qorxmazavto/settings.py
@@ -148,14 +148,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = '/var/www/As_Avto/static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Media files (Uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/As_Avto/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

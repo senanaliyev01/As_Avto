ec87e63d8458ee1229bf70c35fc3d9871dd38ae9
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-04 12:30:51 +0400

    view'lar async kecirildi

--- qorxmazavto/settings.py
+++ qorxmazavto/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'home.middleware.GlobalDataMiddleware',
     'home.middleware.Custom404Middleware',
 ]
 

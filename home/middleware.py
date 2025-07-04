dc7f2d739b70fcce344d94f7c0590e4004e92592
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-04 13:14:16 +0400

    view'lar async kecirildi

--- home/middleware.py
+++ home/middleware.py
@@ -6,6 +6,44 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from home.models import Mehsul
 
+class GlobalDataMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Bütün aktiv mesajları əldə et
+        request.header_messages = Header_Message.objects.filter(aktiv=True).order_by('id')
+
+        if request.user.is_authenticated:
+            # Ümumi borcu əldə edirik
+            statistics = Sifaris.get_order_statistics(request.user)
+            request.statistics = statistics
+
+            # Axtarış üçün lazım olan məlumatları əldə edirik
+            request.categories = Kateqoriya.objects.all()
+            request.brands = Firma.objects.all()
+            request.models = Avtomobil.objects.all()
+
+            # Bütün unikal satıcıları əlavə et
+            seller_ids = Mehsul.objects.exclude(sahib=None).values_list('sahib', flat=True).distinct()
+            sellers = list(User.objects.filter(id__in=seller_ids))
+            # AS-AVTO üçün pseudo-user əlavə et
+            class PseudoUser:
+                def __init__(self, id, username):
+                    self.id = id
+                    self.username = username
+            sellers.insert(0, PseudoUser(0, 'AS-AVTO'))
+            request.sellers = sellers
+
+        # Qlobal statistikalar (bütün səhifələr üçün)
+        request.total_user_count = User.objects.count()
+        request.total_seller_count = Mehsul.objects.exclude(sahib=None).values('sahib').distinct().count()
+        request.total_product_count = Mehsul.objects.count()
+        request.total_firma_count = Firma.objects.count()
+
+        response = self.get_response(request)
+        return response 
+
 class Custom404Middleware:
     def __init__(self, get_response):
         self.get_response = get_response

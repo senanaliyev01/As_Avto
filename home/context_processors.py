686ed0cc9c9105706b6f9250d62b8b2f5f9aa0e9
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-04 12:24:45 +0400

    view'lar async kecirildi

---
+++ home/context_processors.py
@@ -0,0 +1,16 @@
+from asgiref.sync import sync_to_async
+from .models import Header_Message, Kateqoriya, Firma, Avtomobil, Mehsul
+from django.contrib.auth.models import User
+from home.models import Sifaris
+
+async def global_data(request):
+    context = {}
+    context['header_messages'] = await sync_to_async(list)(Header_Message.objects.filter(aktiv=True).order_by('id'))
+    context['total_user_count'] = await sync_to_async(User.objects.count)()
+    context['total_seller_count'] = await sync_to_async(Mehsul.objects.exclude(sahib=None).values('sahib').distinct().count)()
+    context['total_product_count'] = await sync_to_async(Mehsul.objects.count)()
+    context['total_firma_count'] = await sync_to_async(Firma.objects.count)()
+    context['brands'] = await sync_to_async(list)(Firma.objects.all())
+    if request.user.is_authenticated:
+        context['statistics'] = await sync_to_async(Sifaris.get_order_statistics)(request.user)
+    return context 

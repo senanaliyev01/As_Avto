c3e2255d5289eb0ab60b18105b50e3ef05afd2d9
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-09 13:00:29 +0400

    Yorum ve qiymetlendirme

--- home/views.py
+++ home/views.py
@@ -34,6 +34,7 @@ from django.db.models.functions import Concat
 from django.db.models import CharField
 from .models import ProductReview
 from .forms import ProductReviewForm
+from django.db import models
 
 def truncate_product_name(name, max_length=20):
     """Məhsul adını qısaldır və uzun olarsa ... əlavə edir"""

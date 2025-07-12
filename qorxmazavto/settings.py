41264491c96583af6a8e5f0b9fc1a9252d1b6247
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-12 11:34:09 +0400

    sitemap

--- qorxmazavto/settings.py
+++ qorxmazavto/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # Prod üçün False qoyun, testdə True ola bilər
+DEBUG = True  # Prod üçün False qoyun, testdə True ola bilər
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0', 'as-avto.com', 'www.as-avto.com']
 

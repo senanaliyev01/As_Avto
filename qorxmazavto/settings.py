06abf11aa2faf24d769a6beb37fb0e8d418e248e
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-07-19 19:23:21 +0400

    pro reklam yerlesmesi

--- qorxmazavto/settings.py
+++ qorxmazavto/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,6 +145,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ('az', 'Azərbaycan dili'),
+    ('en', 'English'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
